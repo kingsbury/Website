1712593decdd1f25a6d05c73b1e6879425371a54
Casey <kingsbury@users.noreply.github.com>
2017-11-15 15:54:41 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 (function(){
   var servicesContent = '';
   var serviceList = document.registerElement('service-list');
-  var servicesEls = document.getElementsByTagName('service-list');
+  var servicesEls = Array.prototype.slice.call( document.getElementsByTagName('service-list'), 0 );
 
   var services = [
     'Graphic Design',
