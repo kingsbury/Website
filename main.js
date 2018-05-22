300f2d998e3ac73dc992b047162e8863e5b529b3
Casey <kingsbury@users.noreply.github.com>
2018-05-22 08:13:59 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,37 +2,12 @@
   var serviceList = document.registerElement('custom-list');
   var lists = Array.prototype.slice.call( document.getElementsByTagName('custom-list'), 0 );
 
-  this.services = [
-    'Graphic Design',
-    'Illustration',
-    'Web Design',
-    'UI/UX Design',
-    'Other Cool Stuff' 
-  ];
-  
-  this.hobbies = [
-    'VWs',
-    'Art',
-    'Kids',
-    'Tennis',
-    'Other Cool Stuff' 
-  ];
+  this.services = ['Graphic Design','Illustration','Web Design','UI/UX Design','Other Cool Stuff'];
+  this.hobbies = ['VWs','Art','Kids','Tennis','Other Cool Stuff'];
   
   lists.forEach(function(el){
-    var src = el.getAttribute('list');
-    var list = createList(src);
-    el.innerHTML = '<ul>'+list+'<ul>'; 
-    
-    console.log("src", src);
-    console.log("list", list);
-  }); 
-  
-  function createList(listSrc){
-    var content="";
-    this[listSrc].forEach(function(item){
-      content+='<li>'+item+'</li>';
-    });
-    return content;
-  }
-  
+    var list = this[el.getAttribute('list')];
+    var items = list.map(function(item){ return `<li>${item}</li>`; });
+    el.innerHTML = `<ul>${items}<ul>`; 
+  });   
 })();
